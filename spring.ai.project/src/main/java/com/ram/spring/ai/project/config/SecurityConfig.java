aa53616fa0ea7115519e18380a66d6788a7f974a
Ramachandran K <ramkchandran99@gmail.com>
2026-05-15 16:49:38 +0530

    security config file

---
+++ spring.ai.project/src/main/java/com/ram/spring/ai/project/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.ram.spring.ai.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll().anyRequest()
+				.authenticated()).csrf(csrf -> csrf.disable());
+		return http.build();
+	}
+}
